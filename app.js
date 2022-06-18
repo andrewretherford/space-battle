49ec9e5adcc104d0bd1542591356f44eff58c1c8
andrew-retherford <andrewretherford@gmail.com>
2022-06-18 13:13:31 -0700

    Fixed prompts and edited dialogue.  Added some controls to menu and reset playerChoice on each pass

--- app.js
+++ app.js
@@ -47,17 +47,17 @@ class AlienShip extends Ship {
 
     attack = function () {
         if(Math.random() < this.accuracy) {
-            console.log(`The vile alien attacks with deadly precision, dealing ${this.firepower} damage to Earth's last hope!`)
+            alert(`The vile alien attacks with deadly precision, dealing ${this.firepower} damage to Earth's last hope!`)
             return true
         } else {
-            console.log(`The alien fires on you, but the fool's shot misses! It appears that skill in combat is an earthly trait.`)
+            alert(`The alien fires on you, but the fool's shot misses! It appears that skill in combat is an earthly trait.`)
             return false
         }
     }
     gotHit = function (enemyPower) {
         this.hull -= enemyPower
         if(this.hull <= 0) {
-            console.log(`The enemy explodes in a brilliant ball of fire and scrap metal!`)
+            alert(`The enemy explodes in a brilliant ball of fire and scrap metal!`)
         }
     }
 }
@@ -69,17 +69,17 @@ class Schwartzenegger extends Ship {
 
     attack = function () {
         if(Math.random() < this.accuracy) {
-            console.log(`You fire upon your hated enemy and score a direct hit, dealing ${this.firepower} damage!!`)
+            alert(`You fire upon your hated enemy and score a direct hit, dealing ${this.firepower} damage!!`)
             return true
         } else {
-            console.log(`It seems that luck is not with you.  You fire your blasters but your shots go wide.`)
+            alert(`You fire your blasters but your shot goes wide! It would seem that luck does not favor you this day.`)
             return false
         }
     }
     gotHit = function (enemyPower) {
         this.hull -= enemyPower
         if(this.hull <=0) {
-            console.log('This is the end, your ship breaks up around you as alien lasers pierce your hull! Aliens charge through the debris cloud that was the USS Schwartzenegger, last hope of humanity, and speed toward a defensless planet Earth. The desperate populace can only cower as alien lasers rain destruction down from above, superheating the atmosphere and scouring all life from the planet.')
+            alert('This is the end, your ship breaks up around you as alien lasers pierce your hull! Aliens charge through the debris cloud that was the USS Schwartzenegger, last hope of humanity, and speed toward a defensless planet Earth. The desperate populace can only cower as alien lasers rain destruction down from above, superheating the atmosphere and scouring all life from the planet.')
             gameOver = true
         }
     }
@@ -100,46 +100,60 @@ console.log(alienFleet)
 const ussSchwartzenegger = new Schwartzenegger(20, 5, .7)
 console.log(ussSchwartzenegger)
 
-// start game loop
+// declare game global variables
 let gameOver = false
 let playerChoice = ''
 let menu = true
-let prepared, moveOn
+let prepared = ''
+const numberDecoration = ['st', 'nd', 'rd', 'th', 'th', 'th']
 
+// start game loop
 while(!gameOver) {
     // Intro
     if(menu == true) {
-        console.log("Welcome to the end of the world! The aliens are attacking, and only one hero stands between our precious home and certain doom.  You are humanity's last hope! ")
-        // while(prepared !='')
+        alert("Welcome to the end of the world! The aliens are attacking, and only one hero stands between our precious home and annihilation!  You are humanity's last hope. ")
+        while(prepared.toLowerCase() !='y' && prepared.toLowerCase() != 'yes'){
+            prepared = prompt("Are you prepared to face the alien threat?")
+            if(prepared.toLowerCase() == 'n' || prepared.toLowerCase() == 'no') {
+                alert("Alas, Earth's last remaining hero has fled the battlefield along with any hope of survival.  The human race is now extinct!")
+                alert("GAME OVER")
+                gameOver = true
+                break   
+            } 
+        }
         menu = false
     }
 
     // Turn iterator
-    alienFleet.forEach((alienShip, idx, array) => {
-        while(alienShip.hull > 0 && ussSchwartzenegger.hull > 0){
-            if (ussSchwartzenegger.attack()) {  // ussSchwartzenegger turn
-                alienShip.gotHit(ussSchwartzenegger.firepower)
-            }
-            
-            if(alienShip.hull > 0) {    // alien turn
-                if(alienShip.attack()) {
-                    ussSchwartzenegger.gotHit(alienShip.firepower)
+    if(gameOver != true) {
+        alienFleet.forEach((alienShip, idx, array) => {
+            alert(`The ${idx + 1}${numberDecoration[idx]} enemy ship approaches. Time to attack!`)
+            while(alienShip.hull > 0 && ussSchwartzenegger.hull > 0){
+                if (ussSchwartzenegger.attack()) {  // ussSchwartzenegger turn
+                    alienShip.gotHit(ussSchwartzenegger.firepower)
+                }
+                
+                if(alienShip.hull > 0) {    // alien turn
+                    if(alienShip.attack()) {
+                        ussSchwartzenegger.gotHit(alienShip.firepower)
+                    }
                 }
             }
-        }
-        if(idx !== array.length -1) {
-            // console.log(idx)
+            if(idx !== array.length -1) {   // player chooses to fight or retreat if there are still enemies remaining
+                playerChoice = ''
                 while(playerChoice.toLowerCase() != 'fight' && playerChoice.toLowerCase() !='retreat') {
-                    playerChoice = prompt("You have destroyed one of the invaders.  Do you wish to continue the fight or will you retreat and regroup?  Enter 'Fight' to continue, or 'Retreat' to fly home to base: ")
+                    playerChoice = prompt("You have destroyed one of the invaders.  Do you wish to continue the fight or will you retreat and regroup?  Enter 'Fight' to continue, or 'Retreat' to fly home to base.")
                 }                
-            if(playerChoice == 'retreat') {
-                menu = true
+                if(playerChoice == 'retreat') {
+                    menu = true
+                }
+            } else {    // inform player of their victory
+                    alert("You have done it!! Agains all odds you have driven back the alien horde and saved the planet from extraterrestrial wrath! You head home to receive a hero's welcome as world leaders gather to honor your great accomplishment this day. In the years to come, your name will be remembered at the top of the list of humanities greatest champions.")
+                    alert("YOU WIN!!")
+                    gameOver = true
             }
-        } else {
-                alert("You have done it!! Agains all odds you have driven back the alien horde and saved the planet from extraterrestrial wrath! You head home to receive a hero's welcome as world leaders gather to honor your great accomplishment this day. In the years to come, your name will be remembered at the top of the list of humanities greatest champions.")
-                gameOver = true
-        }
-    })
-
-    // gameOver = true // failsafe
+        })
+    } else {
+        break
+    }
 }
